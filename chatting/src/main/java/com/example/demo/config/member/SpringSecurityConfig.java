c3731525ec2cd71bc95f29f5ef6b331218ea1885
CHOIwonik <whwwhs7837@gmail.com>
2024-02-13 15:14:55 +0900

    chat project

--- chatting/src/main/java/com/example/demo/config/member/SpringSecurityConfig.java
+++ chatting/src/main/java/com/example/demo/config/member/SpringSecurityConfig.java
@@ -16,29 +16,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SpringSecurityConfig {
 	@Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-	
-	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable()								// 이 부분은 다시 설정 필요
-                .authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .requestMatchers( "/img/**", "/view/join", "/auth/join").permitAll()
-                        .anyRequest().authenticated()						// 어떠한 요청이라도 인증필요
-                )
-                .formLogin(login -> login									// form 방식 로그인 사용
-                        .loginPage("/view/login")							// custom 로그인페이지
-                        .loginProcessingUrl("/login-process")				// submit을 받을 url
-                        .usernameParameter("userid")						// submit할 아이디
-                        .passwordParameter("pw")							// submit할 비밀번호
-                        .defaultSuccessUrl("/view/dashboard", true)			// 성공 시 '/view/dashboard'로 이동
-                        .failureUrl("/view/loginError")						// 로그인 실패 시
-                        .permitAll()
-                )
-                .logout(withDefaults());									// 로그아웃은 기본설정으로 (/logout으로 인증해제)
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests(request -> request
+					.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+					.requestMatchers( "/img/**", "/view/join", "/auth/join").permitAll()
+					.anyRequest().authenticated()						// 어떠한 요청이라도 인증필요
+					)
+			
+			.formLogin(login -> login									// form 방식 로그인 사용
+					.loginPage("/view/login")							// custom 로그인페이지
+					.loginProcessingUrl("/login-process")				// submit을 받을 url
+					.usernameParameter("userid")						// submit할 아이디
+					.passwordParameter("pw")							// submit할 비밀번호
+					.defaultSuccessUrl("/view/dashboard", true)			// 성공 시 '/view/dashboard'로 이동
+					.failureUrl("/view/loginError")						// 로그인 실패 시
+					.permitAll()
+					)
+			.logout(withDefaults());									// 로그아웃은 기본설정으로 (/logout으로 인증해제)
+
+		return http.build();
+	}
 }
